e1eaf6e90c032cdba1c03bb41996499ca8f35538
diego <d@g081042>
2023-05-11 11:39:38 -0300

    Arquitetura MVC

--- AppListaCurso/app/src/main/java/devandroid/guariz/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/guariz/applistacurso/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.guariz.applistacurso;
+package devandroid.guariz.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.guariz.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
