0fffc3b81de9b3ff2555192b781acb9618fcbae9
diego <d@g081042>
2023-05-17 15:30:03 -0300

    Método DadosParaSpinner

---
+++ AppListaCurso/app/src/main/java/devandroid/guariz/applistacurso/model/Curso.java
@@ -0,0 +1,18 @@
+package devandroid.guariz.applistacurso.model;
+
+public class Curso {
+
+    private String nomeDoCursoDesejado;
+
+    public Curso(String java) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
+
+    public String getNomeDoCursoDesejado() {
+        return nomeDoCursoDesejado;
+    }
+
+    public void setNomeDoCursoDesejado(String nomeDoCursoDesejado) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
+}
--- AppListaCurso/app/src/main/java/devandroid/guariz/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/guariz/applistacurso/view/MainActivity.java
@@ -10,16 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+
 import devandroid.guariz.applistacurso.R;
+import devandroid.guariz.applistacurso.controller.CursoController;
 import devandroid.guariz.applistacurso.controller.PessoaController;
+import devandroid.guariz.applistacurso.model.Curso;
 import devandroid.guariz.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
     PessoaController controller;
 
+    CursoController cursoController;
+
     Pessoa pessoa;
-    Pessoa outraPessoa;
+    List<Curso> listaDeCursos;
 
     EditText editPrimeiroNome;
     EditText editSobrenomeAluno;
@@ -38,12 +44,13 @@ public class MainActivity extends AppCompatActivity {
         controller = new PessoaController(MainActivity.this);
         controller.toString();
 
+        cursoController = new CursoController();
+        listaDeCursos = cursoController.getListaDeCursos();
+
         pessoa = new Pessoa();
 
         controller.buscar(pessoa);
 
-
-        outraPessoa = new Pessoa();
 
         editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
         editSobrenomeAluno = findViewById(R.id.editSobrenomeAluno);
@@ -97,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
-        Log.i("POOAndroid", "Objeto outra pessoa: " + outraPessoa.toString());
 
     }
 }
